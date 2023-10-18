c9cb54a1e903282b528ab45378a4e25937b03f1b
Javi FS <javier.fersal@gmail.com>
2023-10-18 11:13:48 +0000

    Override save method in Product to generate slug

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 
 # Choice Fields
@@ -57,3 +58,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Product, self).save(*args, **kwargs)
