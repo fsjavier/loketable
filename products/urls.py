4b070c9bc16667e7d734c6c79685c8103cd21846
Javi FS <javier.fersal@gmail.com>
2023-11-12 20:49:14 +0000

    Change urls continuation line for consistency

--- products/urls.py
+++ products/urls.py
@@ -16,25 +16,25 @@ urlpatterns = [
         '<int:pk>/<slug:slug>/',
         ProductDetail.as_view(),
         name='product_detail'
-        ),
+    ),
     path(
         'favorite/<int:product_id>/<slug:slug>/',
         ProductListAddFavorite.as_view(),
         name='add_to_favorite'
-        ),
+    ),
     path(
         'edit/<int:pk>/<slug:slug>/',
         EditProduct.as_view(),
         name='edit_product'
-        ),
+    ),
     path(
         'toggle/<int:pk>/<slug:slug>/',
         ToggleProduct.as_view(),
         name='toggle_product'
-        ),
+    ),
     path(
         'delete/<int:pk>/<slug:slug>/',
         DeleteProduct.as_view(),
         name='delete_product'
-        ),
+    ),
 ]
